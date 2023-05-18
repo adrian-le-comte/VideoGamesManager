df71b91aeb3f3a07e4e7f1d7ba9afa3963165e86
Adrian Le Comte <adrian.le-comte@epita.fr>
2023-05-18 15:16:25 +0200

    Added video games database to the project

---
+++ VideoGamesDB/dbo/Tables/users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[users] (
+    [id]         INT            IDENTITY (1, 1) NOT NULL,
+    [username]   NVARCHAR (255) NULL,
+    [password]   NVARCHAR (255) NULL,
+    [role]       NVARCHAR (255) NULL,
+    [created_at] ROWVERSION     NOT NULL,
+    CONSTRAINT [PK__users__3213E83F8498177C] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
