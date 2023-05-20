f4da8fd1cc45935b78d4d4c6e0b4817dd93668dd
HarryBoum <hugo.ravel@orange.fr>
2023-05-20 20:49:04 +0200

    Destock almost done

--- VideoGamesManager/Controllers/DestockController.cs
+++ VideoGamesManager/Controllers/DestockController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using VideoGamesManager.DataAccess.Interfaces;
 using VideoGamesManager.Models;
 
@@ -8,7 +10,6 @@ namespace VideoGamesManager.Controllers
     [Authorize(Policy ="NonAdminOnly")]
     public class DestockController : BaseController
     {
-        private List<Dbo.VideoGame> allSelectedGames = new List<Dbo.VideoGame>();
 
         public DestockController(IVideoGamesRepository videoGameRepository, ICategoryRepository categoryRepository, IStudioRepository studioRepository) : base(videoGameRepository, categoryRepository, studioRepository)
         {
@@ -17,16 +18,38 @@ namespace VideoGamesManager.Controllers
         public IActionResult GenerateBill()
         {
             var allVideoGames = _videoGamesRepository.GetAllVideoGames();
-            var selectedGames = allSelectedGames;
+
+            var selectedGamesJson = TempData["SelectedGames"] as string;
+            var selectedGames = new List<Dbo.VideoGame>();
+            if (selectedGamesJson != null)
+            {
+                selectedGames = JsonConvert.DeserializeObject<List<Dbo.VideoGame>>(selectedGamesJson);
+            }
+            if (selectedGames == null)
+            {
+                selectedGames = new List<Dbo.VideoGame>();
+            }
             DestockViewModel viewModel = new DestockViewModel(allVideoGames, selectedGames);
             return View(viewModel);
         }
 
         [HttpPost]
-        public IActionResult SelectGame(int gameId)
+        public IActionResult AddGame(string Name, string Price, string Stock, string gameId)
         {
-            var game = _videoGamesRepository.GetVideoGameById(gameId);
-            allSelectedGames.Add(game);
+            var id = int.Parse(gameId);
+            var game = _videoGamesRepository.GetVideoGameById(id);
+            game.Stock = int.Parse(Stock);
+            var selectedGames = new List<Dbo.VideoGame>();
+            var selectedGamesJson = TempData["SelectedGames"] as string;
+            if (!string.IsNullOrEmpty(selectedGamesJson))
+            {
+                selectedGames = JsonConvert.DeserializeObject<List<Dbo.VideoGame>>(selectedGamesJson);
+            }
+
+            selectedGames.Add(game);
+
+            selectedGamesJson = JsonConvert.SerializeObject(selectedGames);
+            TempData["SelectedGames"] = selectedGamesJson;
             return RedirectToAction("GenerateBill");
         }
 
--- VideoGamesManager/Models/DestockViewModel.cs
+++ VideoGamesManager/Models/DestockViewModel.cs
@@ -6,6 +6,7 @@ namespace VideoGamesManager.Models
     {
         public List<VideoGame> Games { get; set; }
         public List<VideoGame> SelectedGames { get; set; }
+        public int AddedGameId { get; set; }
 
         public DestockViewModel(List<VideoGame> games, List<VideoGame> selectedGames) {
             Games = games;
