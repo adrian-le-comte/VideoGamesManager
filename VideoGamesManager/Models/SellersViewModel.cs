aee32618e52af97811a877ccab573f867923c65a
Adrian Le Comte <adrian.le-comte@epita.fr>
2023-05-20 22:29:04 +0200

    Added View models for sellers

---
+++ VideoGamesManager/Models/SellersViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace VideoGamesManager.Models
+{
+    public class SellersViewModel
+    {
+        public SellerViewModel Seller { get; set; }
+        public List<Models.SellerViewModel> Sellers { get; set; }
+    }
+}
