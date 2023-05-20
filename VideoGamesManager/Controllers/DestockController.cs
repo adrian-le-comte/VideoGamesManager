e04f6df50f9fe8c6d8709d952478df68e7bdbd57
HarryBoum <hugo.ravel@orange.fr>
2023-05-20 14:41:30 +0200

    Comit

--- VideoGamesManager/Controllers/DestockController.cs
+++ VideoGamesManager/Controllers/DestockController.cs
@@ -1,19 +1,34 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VideoGamesManager.DataAccess.Interfaces;
+using VideoGamesManager.Models;
 
 namespace VideoGamesManager.Controllers
 {
     [Authorize(Policy ="NonAdminOnly")]
     public class DestockController : BaseController
     {
+        private List<Dbo.VideoGame> allSelectedGames = new List<Dbo.VideoGame>();
+
         public DestockController(IVideoGamesRepository videoGameRepository, ICategoryRepository categoryRepository, IStudioRepository studioRepository) : base(videoGameRepository, categoryRepository, studioRepository)
         {
         }
 
         public IActionResult GenerateBill()
         {
-            return View();
+            var allVideoGames = _videoGamesRepository.GetAllVideoGames();
+            var selectedGames = allSelectedGames;
+            DestockViewModel viewModel = new DestockViewModel(allVideoGames, selectedGames);
+            return View(viewModel);
         }
+
+        [HttpPost]
+        public IActionResult SelectGame(int gameId)
+        {
+            var game = _videoGamesRepository.GetVideoGameById(gameId);
+            allSelectedGames.Add(game);
+            return RedirectToAction("GenerateBill");
+        }
+
     }
 }
---
+++ VideoGamesManager/Models/DestockViewModel.cs
@@ -0,0 +1,15 @@
+﻿using VideoGamesManager.Dbo;
+
+namespace VideoGamesManager.Models
+{
+    public class DestockViewModel
+    {
+        public List<VideoGame> Games { get; set; }
+        public List<VideoGame> SelectedGames { get; set; }
+
+        public DestockViewModel(List<VideoGame> games, List<VideoGame> selectedGames) {
+            Games = games;
+            SelectedGames = selectedGames;
+        }
+    }
+}
